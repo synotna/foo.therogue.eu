82b29a6e8c4c28179f781e11b3f8f7eb7ad426bc
synotna <synotna@users.noreply.github.com>
2023-05-26 11:39:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log(window.frames[0]);
 console.log(window.frames[0].contentWindow);
 console.log(window.frames[0].document);
+console.log(window.frames[0].frames.document.body.style.background);
